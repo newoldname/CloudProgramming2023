70d32c53c9b3c8498609b0e6dc87ba672420752a
newoldname <thjbjs@outlook.com>
2023-04-04 16:23:53 +0900

    [feat] 카테고리, 작성자 저장 기능

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,24 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
@@ -14,7 +26,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_self_link(self):
         return f'/blog/{self.pk}/'
